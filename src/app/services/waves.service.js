bb277c0f6669698c9b0d2b56f116768a2527593b
stacktracer <vostroknutov@hotmail.com>
2019-05-19 15:47:06 +0300

    fix waves keeper service

--- src/app/services/waves.service.js
+++ src/app/services/waves.service.js
@@ -1,41 +1,41 @@
 export const WavesKeeperClient = (wavesKeeper => {
 
-    const isAvailable = Boolean(wavesKeeper);
+    const isAvailable = () => Boolean(wavesKeeper);
     const availableError = 'Waves keeper is not available, please install browser extension.'
 
     return {
-        isAvailable: () => isAvailable,
+        isAvailable: () => isAvailable(),
         availableError: () => availableError,
         /**@returns {Promise} */
-        auth: (...args) => isAvailable ? wavesKeeper.auth(...args) : Promise.reject(availableError),
+        auth: (...args) => isAvailable() ? wavesKeeper.auth(...args) : Promise.reject(availableError),
         /**@returns {Promise} */
-        initialPromise: (...args) => isAvailable ? wavesKeeper.initialPromise(...args) : Promise.reject(availableError),
+        initialPromise: (...args) => isAvailable() ? wavesKeeper.initialPromise(...args) : Promise.reject(availableError),
         /**@returns {Promise} */
-        notification: (...args) => isAvailable ? wavesKeeper.notification(...args) : Promise.reject(availableError),
+        notification: (...args) => isAvailable() ? wavesKeeper.notification(...args) : Promise.reject(availableError),
         on: (...args) => {
-            if (isAvailable) {
+            if (isAvailable()) {
                 return wavesKeeper.on(...args);
             }
             throw new Error(availableError);
         },
         /**@returns {Promise} */
-        publicState: (...args) => isAvailable ? wavesKeeper.publicState(...args) : Promise.reject(availableError),
+        publicState: (...args) => isAvailable() ? wavesKeeper.publicState(...args) : Promise.reject(availableError),
         /**@returns {Promise} */
-        signAndPublishCancelOrder: (...args) => isAvailable ? wavesKeeper.signAndPublishCancelOrder(...args) : Promise.reject(availableError),
+        signAndPublishCancelOrder: (...args) => isAvailable() ? wavesKeeper.signAndPublishCancelOrder(...args) : Promise.reject(availableError),
         /**@returns {Promise} */
-        signAndPublishOrder: (...args) => isAvailable ? wavesKeeper.signAndPublishOrder(...args) : Promise.reject(availableError),
+        signAndPublishOrder: (...args) => isAvailable() ? wavesKeeper.signAndPublishOrder(...args) : Promise.reject(availableError),
         /**@returns {Promise} */
-        signAndPublishTransaction: (...args) => isAvailable ? wavesKeeper.signAndPublishTransaction(...args) : Promise.reject(availableError),
+        signAndPublishTransaction: (...args) => isAvailable() ? wavesKeeper.signAndPublishTransaction(...args) : Promise.reject(availableError),
         /**@returns {Promise} */
-        signCancelOrder: (...args) => isAvailable ? wavesKeeper.signCancelOrder(...args) : Promise.reject(availableError),
+        signCancelOrder: (...args) => isAvailable() ? wavesKeeper.signCancelOrder(...args) : Promise.reject(availableError),
         /**@returns {Promise} */
-        signOrder: (...args) => isAvailable ? wavesKeeper.signOrder(...args) : Promise.reject(availableError),
+        signOrder: (...args) => isAvailable() ? wavesKeeper.signOrder(...args) : Promise.reject(availableError),
         /**@returns {Promise} */
-        signRequest: (...args) => isAvailable ? wavesKeeper.signRequest(...args) : Promise.reject(availableError),
+        signRequest: (...args) => isAvailable() ? wavesKeeper.signRequest(...args) : Promise.reject(availableError),
         /**@returns {Promise} */
-        signTransaction: (...args) => isAvailable ? wavesKeeper.signTransaction(...args) : Promise.reject(availableError),
+        signTransaction: (...args) => isAvailable() ? wavesKeeper.signTransaction(...args) : Promise.reject(availableError),
         /**@returns {Promise} */
-        signTransactionPackage: (...args) => isAvailable ? wavesKeeper.signTransactionPackage(...args) : Promise.reject(availableError)
+        signTransactionPackage: (...args) => isAvailable() ? wavesKeeper.signTransactionPackage(...args) : Promise.reject(availableError)
     }
 
 })(window.WavesKeeper);
